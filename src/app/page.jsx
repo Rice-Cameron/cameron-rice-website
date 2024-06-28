631fb8a7a2bfaec31ac5a86acc6fc015b51da402
Rice-Cameron <cameron5237@gmail.com>
2024-06-28 12:08:52 -0700

    test pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Link from 'next/link';
 
-export default function page() {
+export default function home() {
     return (
         <section className='py-24'>
             <div className='container'>
-                <h1 className='text-4xl font-bold'>Page</h1>
+                <h1 className='text-4xl font-bold'>Home</h1>
                 <Link href='/users'>Users</Link>
             </div>
         </section>
