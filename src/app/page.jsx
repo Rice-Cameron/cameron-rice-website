5e01d5f96f6baa11b8fc030b3a7ecc31063e0a44
Rice-Cameron <cameron5237@gmail.com>
2024-06-28 12:03:44 -0700

    mess around with next, ignore .github/ for now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Link from 'next/link';
 
 export default function page() {
     return (
         <section className='py-24'>
             <div className='container'>
                 <h1 className='text-4xl font-bold'>Page</h1>
+                <Link href='/users'>Users</Link>
             </div>
         </section>
     )
