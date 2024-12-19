6d440b2480df6d21c96cb16cc4c5374baf07becc
Rice-Cameron <cameron5237@gmail.com>
2024-12-18 16:55:03 -0800

    Implement basic pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
 import Link from 'next/link';
 
-export default function home() {
+export default function Home() {
     return (
-        <section className='py-24'>
-            <div className='container'>
-                <h1 className='text-4xl font-bold'>Home</h1>
-                <Link href='/users'>Users</Link>
-            </div>
-        </section>
-    )
+        <div className="home-container">
+            <h1 className="home-title">Cameron Rice - Web Developer</h1>
+            <p className="home-intro">Welcome to my portfolio! Here are some of my projects:</p>
+            <ul className="project-list">
+                <li>
+                    <Link href="/projects/project1" legacyBehavior>
+                        <a className="project-link">Project 1: Description</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/projects/project2" legacyBehavior>
+                        <a className="project-link">Project 2: Description</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/projects/project3" legacyBehavior>
+                        <a className="project-link">Project 3: Description</a>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
---
+++ src/app/projects/project1.jsx
@@ -0,0 +1,10 @@
+export default function Project1() {
+    return (
+        <div>
+            <h1>Project 1 Title</h1>
+            <p>Description of Project 1.</p>
+            <p>Technologies used: React, Next.js, etc.</p>
+            <a href="https://github.com/yourusername/project1" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+        </div>
+    );
+} 
